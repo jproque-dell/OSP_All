1edc249da15b6c6248dcfdc58accc9ebb6a0a178
Vincent S. Cojot <vcojot@redhat.com>
2017-03-28 14:38:45 -0400

    Minor updates from upstream...

--- osp10/all-post-config/scripts/pcs_cluster_restart.sh
+++ osp10/all-post-config/scripts/pcs_cluster_restart.sh
@@ -9,32 +9,19 @@ max_ctrl_delay=180
 case "${my_node_role}${my_node_index}" in
         "CTRL0")
 		echo "Doing PCS-specific restarts on Controller0..."
-		#pcs cluster stop --all
-		#pcs cluster start --all
+		pcs cluster stop --all
+		pcs cluster start --all
+		#pcs resource restart rabbitmq-clone
 	;;
 esac
 
 # Wait some time as to avoid taking all services down on all controllers at the same time
 case "${my_node_role}${my_node_index}" in
-        "CTRL0")
-		/usr/bin/systemctl mask openstack-nova-compute
+        "CTRL0"|"CTRL1"|"CTRL2")
 		local_delay=$((${max_ctrl_delay} * ${my_node_index}))
 		echo "Waiting for ${local_delay} secs on Controller0..."
 		sleep ${local_delay}
 		sleep $(($(od -A n -t d -N 3 /dev/urandom) % 60))
-	;;
-        "CTRL1")
-		/usr/bin/systemctl mask openstack-nova-compute
-		local_delay=$((${max_ctrl_delay} * ${my_node_index}))
-		echo "Waiting for ${local_delay} secs on Controller1..."
-		sleep ${local_delay}
-		sleep $(($(od -A n -t d -N 3 /dev/urandom) % 60))
-	;;
-        "CTRL2")
-		/usr/bin/systemctl mask openstack-nova-compute
-		local_delay=$((${max_ctrl_delay} * ${my_node_index}))
-		echo "Waiting for ${local_delay} secs on Controller0..."
-		sleep ${local_delay}
 	;;
 esac
 
@@ -42,18 +29,19 @@ esac
 case ${my_node_role} in
 	CTRL)
 		echo "Doing Controller-specific restarts..."
-		systemctl restart neutron-dhcp-agent neutron-l3-agent neutron-metadata-agent neutron-openvswitch-agent
+		/usr/bin/systemctl mask openstack-nova-compute
+		systemctl restart neutron-server neutron-openvswitch-agent neutron-metadata-agent neutron-l3-agent neutron-dhcp-agent
 		systemctl restart openstack-\*
 		systemctl restart httpd
 	;;
-        CMPT)
+	CMPT)
 		echo "Doing Compute-specific restarts..."
 		sleep $(($(od -A n -t d -N 3 /dev/urandom) % 120))
-		systemctl restart neutron-openvswitch-agent.service
+		systemctl restart neutron-\*
 		sleep $(($(od -A n -t d -N 3 /dev/urandom) % 120))
 		systemctl restart openstack-\*
 	;;
-        CEPH)
+	CEPH)
 		echo "Doing Ceph-specific restarts..."
 		sleep $(($(od -A n -t d -N 3 /dev/urandom) % 120))
 		systemctl restart openstack-\*
--- tests/_oc.sh
+++ tests/_oc.sh
@@ -18,3 +18,16 @@ if [ -f ${HOME}/internalrc ]; then
 	. ${HOME}/internalrc
 fi
 nova boot novavm --flavor m1.micro --image "cirros" --nic net-id=${NET_ID} --min-count 8
+
+for i in {1..8}
+do
+	echo -n "Waiting for novavm-${i} to become ACTIVE...."
+	until [[ "$(nova show novavm-${i} | awk '/status/ {print $4}')" == "ACTIVE" ]]; do
+		echo -n "."
+	done
+	echo -ne "OK\n"
+	myip=$(openstack server list -c Networks -f value --name novavm-${i}|cut -d= -f2|sed -e 's/,.*//')
+	myport=$(neutron port-list --fixed_ips ip_address=${myip} -c id -f value)
+	myfloat=$(openstack floating ip list -f value -c ID|sed -e "${i}q;d")
+	neutron floatingip-associate $myfloat $myport
+done
