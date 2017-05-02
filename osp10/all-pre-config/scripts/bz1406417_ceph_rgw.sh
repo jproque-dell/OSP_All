57202a5eeff5519a3155decbd90e540ee6683e1c
Vincent S. Cojot <vcojot@redhat.com>
2017-05-02 15:31:26 -0400

    Workaround for Rados GW issue + Readonly role implementation

--- osp10/all-pre-config/scripts/bz1406417_ceph_rgw.sh
+++ osp10/all-pre-config/scripts/bz1406417_ceph_rgw.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 #
-set -x ; VLOG=/var/log/ospd/pre_deploy-allow_innodb_file_per_table.log ; exec &> >(tee -a "${VLOG}")
+set -x ; VLOG=/var/log/ospd/pre_deploy-bz1406417_ceph_rgw.log ; exec &> >(tee -a "${VLOG}")
 my_node_role=$(cat /var/log/ospd/node_role)
-patch_file=/var/tmp/bz1406417_gnocchi.patch
+patch_file=/var/tmp/bz1406417_ceph_rgw.patch
 #
 case ${my_node_role} in
 	*)
-		if [ -f /usr/lib/python2.7/site-packages/gnocchi/storage/ceph.py ]; then
+		if [ -f /usr/lib/python2.7/site-packages/ceph_rgw/storage/ceph.py ]; then
 cat > ${patch_file} << EOF
-*** /usr/lib/python2.7/site-packages/gnocchi/storage/ceph.py.orig       2017-03-10 21:46:05.000000000 +0000
---- /usr/lib/python2.7/site-packages/gnocchi/storage/ceph.py    2017-04-12 13:50:13.295146413 +0000
+*** /usr/lib/python2.7/site-packages/ceph_rgw/storage/ceph.py.orig       2017-03-10 21:46:05.000000000 +0000
+--- /usr/lib/python2.7/site-packages/ceph_rgw/storage/ceph.py    2017-04-12 13:50:13.295146413 +0000
 ***************
 *** 50,55 ****
 --- 50,56 ----   
@@ -37,7 +37,7 @@ EOF
 			if [ -f ${patch_file} ]; then
 				sudo patch -p0 < \
 				${patch_file} \
-				/usr/lib/python2.7/site-packages/gnocchi/storage/ceph.py
+				/usr/lib/python2.7/site-packages/ceph_rgw/storage/ceph.py
 			fi
 		fi
   ;;
